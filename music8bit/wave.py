518af8f69be6264539f9fa2bde3c573bcf401614
neutrino-dot <iz24919bd@isk.ed.jp>
2025-09-25 18:45:37 +0900

    追加のライブラリ

--- music8bit/wave.py
+++ music8bit/wave.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import signal
 from abc import ABC, abstractmethod
 from .core import SongConfig
+import numbers
 class WaveGenerator(ABC):
     """
     Abstract base class for waveform generators.
