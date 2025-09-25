7ca409e6e0485522525dab912a3d4be0bab0a284
neutrino-dot <iz24919bd@isk.ed.jp>
2025-09-25 19:07:49 +0900

    a9

--- music8bit/__init__.py
+++ music8bit/__init__.py
@@ -1,11 +1,13 @@
 from .core import Part, SongMixer # 主要クラスを表に出す
 from .utils import play_audio      # 再生関数も表に出す
-from .wave import WaveGenerator,SquareWave,TriangleWave
+from .wave import *
 
 __all__ = [
     "SongMixer",
     "Part",
     "play_audio",
     "SquareWave",
-    "TriangleWave"
+    "TriangleWave",
+    "NoiseWave",
+    "WaveGenerator"
 ]
--- music8bit/core.py
+++ music8bit/core.py
@@ -2,7 +2,7 @@ import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
-from .wave import WaveGenerator
+from .wave import WaveGenerator, SquareWave, SineWave, TriangleWave, NoiseWave
 import numbers
 
 
