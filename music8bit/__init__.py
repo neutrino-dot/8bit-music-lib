520d0c2bb44c0dfc0c539d21b9a654226bf724c0
neutrino-dot <iz24919bd@isk.ed.jp>
2025-09-25 18:27:15 +0900

    a1

--- music8bit/__init__.py
+++ music8bit/__init__.py
@@ -1,6 +1,6 @@
 from .core import SongMixer, Part  # 主要クラスを表に出す
 from .utils import play_audio      # 再生関数も表に出す
-from .wave import SquareWave,TriangleWave
+from .wave import WaveGenerator,SquareWave,TriangleWave
 
 __all__ = [
     "SongMixer",
