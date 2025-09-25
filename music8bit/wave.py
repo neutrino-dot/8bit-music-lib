26b787f219651a27f46f16e2a680f2d70f46284c
neutrino-dot <iz24919bd@isk.ed.jp>
2025-09-25 18:40:24 +0900

    a5

--- music8bit/wave.py
+++ music8bit/wave.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import signal
 from abc import ABC, abstractmethod
-from SongConfig import checkVT
+from .SongConfig import checkVT
 class WaveGenerator(ABC):
     """
     Abstract base class for waveform generators.
