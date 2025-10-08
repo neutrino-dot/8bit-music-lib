14f77cbba189d2031bbdb2e20fb5ea01134661f3
neutrino-dot <iz24919bd@isk.ed.jp>
2025-10-08 21:02:12 +0900

    Update __init__.py

--- music8bit/__init__.py
+++ music8bit/__init__.py
@@ -1,11 +1,9 @@
-from .core import Part, SongMixer 
-from .utils import play_audio     
+from .core import Part, SongMixer   
 from .wave import *
 
 __all__ = [
     "SongMixer",
     "Part",
-    "play_audio",
     "SquareWave",
     "TriangleWave",
     "SineWave",
